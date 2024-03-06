5134f14c0a495843276030491f7d78103839945e
0XReagan <0XReagan@proton.me>
2024-03-06 16:01:39 -0300

    init

--- app.py
+++ app.py
@@ -83,38 +83,38 @@ def hello_world():
 #     return Response(svg_data, mimetype='image/svg+xml')
 
     
-# # Define the GET endpoint
-# @app.route('/tickets/<int:ticket_id>/events/<int:event_id>', methods=['GET'])
-# def get_ticket_info(ticket_id, event_id):
+# Define the GET endpoint
+@app.route('/tickets/<int:ticket_id>/events/<int:event_id>', methods=['GET'])
+def get_ticket_info(ticket_id, event_id):
 
-#     now = datetime.now()
-#     scan_time = now.strftime(" %I:%M:%S %p | %Y-%m-%d")
+    now = datetime.now()
+    scan_time = now.strftime(" %I:%M:%S %p | %Y-%m-%d")
 
-#     embed = {
-#         "title": "🚀",
-#         "description": f"Event ID: {event_id}\nTicket ID: {ticket_id}\nScan Time: {scan_time}\n\n{SERVVER_URL}",
-#         "color": 1543684, 
-#         "fields": [],
-#         "footer": {
-#             "text": "** use report URL to get a text listing of all activity"
-#         }
-#     }
+    embed = {
+        "title": "🚀",
+        "description": f"Event ID: {event_id}\nTicket ID: {ticket_id}\nScan Time: {scan_time}\n\n{SERVVER_URL}",
+        "color": 1543684, 
+        "fields": [],
+        "footer": {
+            "text": "** use report URL to get a text listing of all activity"
+        }
+    }
 
-#     # Wrap the embed in a payload as Discord expects
-#     payload = {
-#         "embeds": [embed],
-#     }
+    # Wrap the embed in a payload as Discord expects
+    payload = {
+        "embeds": [embed],
+    }
 
-#     # Convert the payload to JSON and make the POST request to the webhook URL
-#     response = requests.post(WEBHOOK_URL, json=payload)
+    # Convert the payload to JSON and make the POST request to the webhook URL
+    response = requests.post(WEBHOOK_URL, json=payload)
 
-#     # Check the response
-#     if response.status_code == 204:
-#         print("Embed sent successfully!")
-#     else:
-#         print(f"Failed to send embed. Status code: {response.status_code} - Response: {response.text}")
+    # Check the response
+    if response.status_code == 204:
+        print("Embed sent successfully!")
+    else:
+        print(f"Failed to send embed. Status code: {response.status_code} - Response: {response.text}")
 
-#     return 'Hello, world!'
+    return 'Hello, world!'
 
     
 if __name__ == '__main__':
