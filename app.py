73f8f032b28bc404e93417b780a7aea7cc9cb366
0XReagan <0XReagan@proton.me>
2024-03-06 16:30:47 -0300

    init

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import json
 # Your Discord webhook URL
 WEBHOOK_URL = "https://discord.com/api/webhooks/1214662183452016660/1yOSpSVg3oj0gr6rQWnpKW9ncjt-TKeODdlzXE12hWSLwmNlUNOEUI21L3hmxPYCvK5u"
 SERVVER_URL = 'https://drab-gold-chimpanzee-shoe.cyclic.app'
+APPROVED_SVG = 'approved.svg'
 
 app = Flask(__name__)
 
@@ -19,28 +20,40 @@ def hello_world():
 
 @app.route('/submit', methods=['GET'])
 def submit_request():
+    now = datetime.now()
+    scan_time = now.strftime(" %I:%M:%S %p | %Y-%m-%d")
     event_id = request.args.get('event_id')
     ticket_id = request.args.get('ticket_id')
 
     if not event_id or not ticket_id:
-        return Response("{'error': 'Missing event_id or ticket_id'}", status=400, mimetype='application/json')
+            return Response("{'error': 'Missing event_id or ticket_id'}", status=400, mimetype='application/json')
 
-    # # Construct the URL with the event_id and ticket_id
-    # target_url = f"{SERVER_URL}/some-endpoint?event_id={event_id}&ticket_id={ticket_id}"
+    embed = {
+        "title": "🚀",
+        "description": f"Event ID: {event_id}\nTicket ID: {ticket_id}\nScan Time: {scan_time}\n\n{SERVVER_URL}",
+        "color": 1543684, 
+        "fields": [],
+        "footer": {
+            "text": "** use report URL to get a text listing of all activity"
+        }
+    }
 
-    # # Make a GET request to the server
-    # response = requests.get(target_url)
+    # Wrap the embed in a payload as Discord expects
+    payload = {
+        "embeds": [embed],
+    }
 
-    # if response.status_code == 200:
-    #     # You can process the response here if needed
-    #     return Response(response.content, status=200, mimetype='application/json')
-    # else:
-    #     return Response("{'error': 'Failed to get data from server'}", status=response.status_code, mimetype='application/json')
+    # Convert the payload to JSON and make the POST request to the webhook URL
+    response = requests.post(WEBHOOK_URL, json=payload)
 
-    # return f'done! {ticket_id} {event_id}'
+    # Check the response
+    if response.status_code == 204:
+        print("Embed sent successfully!")
+    else:
+        print(f"Failed to send embed. Status code: {response.status_code} - Response: {response.text}")
 
    # You might want to select an SVG file dynamically based on ticket_id and event_id
-    svg_file_path = 'approved.svg'
+    svg_file_path = APPROVED_SVG
     
     # Open the SVG file and read its contents
     with open(svg_file_path, 'r') as svg_file:
